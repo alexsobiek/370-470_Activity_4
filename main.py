9a7e9a7e6127cd2bf731549ee918d634d58a4146
Justin Atteberry <justin_atteberry@yahoo.com>
2022-04-12 19:44:04 -0500

    Added merge sort and main

---
+++ main.py
@@ -0,0 +1,5 @@
+from mergesort import mergeSort, userInput
+
+r = userInput()
+mergeSort(r)
+print(r)
---
+++ mergesort.py
@@ -0,0 +1,38 @@
+def mergeSort(array):
+    if len(array) > 1:
+      mid = len(array) // 2
+      leftSection = array[:mid]
+      rightSection = array[mid:]
+
+      mergeSort(leftSection)
+      mergeSort(rightSection)
+
+      i = 0
+      j = 0
+      k = 0
+
+      while i < len(leftSection) and j < len(rightSection):
+        if leftSection[i] < rightSection[j]:
+          array[k] = leftSection[i]
+          i += 1
+        else:
+          array[k] = rightSection[j]
+          j += 1
+        k += 1
+      while i < len(leftSection):
+        array[k] = leftSection[i]
+        i += 1
+        k += 1
+      while j < len(rightSection):
+        array[k] = rightSection[j]
+        j += 1
+        k += 1
+    return array
+
+def userInput():
+    inputArray = []
+    n = int(input("Enter size of array: "))
+    for i in range(0,n):
+      element = int(input())
+      inputArray.append(element)
+    return inputArray
